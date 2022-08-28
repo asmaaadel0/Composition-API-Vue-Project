3e0783cf2fe0dc77c350adc87f599413bb1d8910
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-28 14:33:20 +0200

    Update ProjectsList.vue

--- src/components/projects/ProjectsList.vue
+++ src/components/projects/ProjectsList.vue
@@ -31,14 +31,14 @@ export default {
 
     const { enteredSearchTerm, availableItems, updateSearch } = useSearch(projects, 'title');
     const hasProjects = computed(function () {
-      return props.user.projects && availableItems.value.length > 0;
+      return user.value.projects && availableItems.value.length > 0;
     });
 
 
 
 
     watch(user, function () {
-      enteredSearchTerm.value = '';
+      updateSearch("");
     });
 
     return {
